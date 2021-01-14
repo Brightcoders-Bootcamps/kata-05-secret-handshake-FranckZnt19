4e2c2e1bd6269c67ba933d6e9f359fce562e633f
Frania Suarez <frania.sinahi.suarez.lopez@gmail.com>
2021-01-13 18:47:48 -0600

    add class Num and Handshake for the function of the code with puts test

--- handshake.rb
+++ handshake.rb
@@ -1,1 +1,52 @@
-puts "Hola mundo"
+class Num
+  attr_accessor :numero
+
+  def initialize(numero)
+    @numero = numero
+  end
+
+  def mayor?
+    return true if @numero >= 16
+  end
+
+  def menor?
+    return true if @numero < 16
+  end
+
+  def binary
+    @numero.to_s(2)
+  end
+end
+
+class Handshake
+  def evaluation
+    secret = []
+    numbinary = binary
+    separacion = binary.chars.reverse
+
+    separacion.each do |bin|
+      secret << bin
+    end
+
+    if @numero.mayor?
+      
+
+    end
+  end
+end
+
+#------
+secret_handshake = Num.new(20)
+puts secret_handshake.mayor?
+puts secret_handshake.menor?
+puts secret_handshake.binary
+puts secret_handshake.binary.reverse
+
+palabras = secret_handshake.binary.chars.reverse
+# Igualmente se puede recorrer
+secret = []
+palabras.each do |palabra|
+	secret << palabra
+end
+
+puts secret[0]
